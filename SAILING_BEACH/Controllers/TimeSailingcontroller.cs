ff80adc01962ff20e7aa3cc080eb4835dc820cd6
chani <chani05480@gmail.com>
2024-06-03 17:10:12 +0300

    my commit message

---
+++ SAILING_BEACH/Controllers/TimeSailingcontroller.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Sailing.Api.Models;
+using Sailing.Core;
+using Sailing.Core.DTOs;
+using Sailing.Core.Service;
+using Sailing.Service;
+using SAILING_BEACH;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sailing.Api.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeSailingcontroller : ControllerBase
+    {
+        private readonly ISalingTimeService _timeService;
+        private readonly IMapper _mapper;
+        // private readonly Mapping _mapping;
+        public TimeSailingcontroller(ISalingTimeService timeService, IMapper mapper)
+        {
+            _timeService = timeService;
+            _mapper = mapper;       
+        }
+
+        // GET: api/<TimeSailingcontroller>
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var SailingTime = _timeService.GetSailingTimeAsync();
+
+            var SailingTimeDto=_mapper.Map<List<SailingTimeDto>>(SailingTime);
+         
+            return Ok(SailingTimeDto);
+        }
+
+        // GET api/<TimeSailingcontroller>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var getIdTask = await _timeService.GetSailingTimeByIdAsync(id);
+         
+            var getIdDto = _mapper.Map<SailingTimeDto>(getIdTask);
+           
+            return Ok(getIdDto);
+        }
+
+        // POST api/<TimeSailingcontroller>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] SailingTime sailingTime)
+        {
+            var addSailingTime = await _timeService.AddSailingTimeAsync(sailingTime);
+
+            var addSailingTimeDto = _mapper.Map<SailingTimeDto>(addSailingTime);
+
+            return Ok(addSailingTimeDto);
+        }
+
+        // PUT api/<TimeSailingcontroller>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult >Put(int id, [FromBody] SailingTime sailingTime)
+        {
+            var UPdatesailingTime = await _timeService.UpdateSailingTimeAsync(id, sailingTime);
+
+            var UPdateSailingTimerDto = _mapper.Map<SailingTimeDto>(UPdatesailingTime);
+
+            return Ok(UPdateSailingTimerDto);
+
+
+           
+        }
+        // DELETE api/<TimeSailingcontroller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _timeService.DeleteSailingTime(id);
+        }
+    }
+}
---
+++ Sailing.Data/Repository/TimeSailingRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sailing.Core.Repository;
+using SAILING_BEACH;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sailing.Data.Repository
+{
+    public class TimeSailingRepository : ISailingTimeRepository
+    {
+
+        private readonly Context _context;
+        public TimeSailingRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public List<SailingTime> GetSailingTime()
+        {
+            return _context.SailingTime.ToList();
+        }
+
+        public  async Task<SailingTime> GetSailingTimeById(int id)
+        {
+            var sailingTime = _context.SailingTime.Find(id);
+            await _context.SaveChangesAsync();
+            return sailingTime;
+        }
+
+        public async Task<SailingTime> AddSailingTime(SailingTime sailingTime)
+        {
+            _context.SailingTime.Add(sailingTime);
+            await _context.SaveChangesAsync();
+
+            return sailingTime;
+            
+        }
+
+        public async Task<SailingTime> UpdateSailingTime(int id, SailingTime sailingTime1)
+        {
+            var sailingtime=_context.SailingTime.Find(id);
+            sailingtime.StartTime = sailingTime1.StartTime;
+            sailingtime.EndTime = sailingTime1.EndTime;
+            sailingtime.Boat=sailingTime1.Boat;
+            sailingtime.BoatDriver=sailingTime1.BoatDriver;
+
+            await _context.SaveChangesAsync();
+            return sailingtime;
+        }
+
+        public void DeleteSailingTime(int id)
+        {
+            _context.SailingTime.Remove(_context.SailingTime.Find(id));
+             _context.SaveChanges();
+        }
+    }
+}
